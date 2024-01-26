1504eae2af05ff404d2729ebfa2340f5219cfbe4
azismaulik <azismaulik0@gmail.com>
2024-01-26 14:40:34 +0700

    first commit

---
+++ src/_root/RootLayout.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+const RootLayout = () => {
+  return (
+    <div>
+      <Button>Button</Button>
+    </div>
+  );
+};
+
+export default RootLayout;
---
+++ src/lib/validation/index.ts
@@ -0,0 +1,12 @@
+import * as z from "zod";
+
+export const SignupValidation = z.object({
+  name: z.string().min(4, { message: "Name must be at least 4 characters" }),
+  username: z
+    .string()
+    .min(2, { message: "Name must be at least 2 characters" }),
+  email: z.string().email({ message: "Invalid email address" }),
+  password: z
+    .string()
+    .min(6, { message: "Password must be at least 6 characters." }),
+});
